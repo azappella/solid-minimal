3ddac048410baf1bc6d3df937cee92a44e92f567
Andrew Zappella <andrew.zappella@gmail.com>
2019-09-25 12:41:28 +0200

    Adding main file and entrypoint for solid application

--- src/index.js
+++ src/index.js
@@ -1,23 +1,99 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createGlobalStyle } from 'styled-components';
+import { createSignal, onCleanup } from 'solid-js';
+import { render } from 'solid-js/dom';
+import { createBrowserHistory } from 'history';
+import { Link } from './components/link';
+import { Router, useRouter } from './components/router';
 
-import store from './store';
-import App from './components/app';
-import normalize from './css/normalize';
+const history = createBrowserHistory();
 
-const GlobalStyle = createGlobalStyle`${normalize}`;
+function navigate(e) {
+  e.preventDefault();
+  return (location) => history.push(`${location}`);
+}
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Fragment>
-            <Provider store={store}>
-                <App />
-            </Provider>
-            <GlobalStyle />
-        </Fragment>
-    </BrowserRouter>,
-    document.getElementById('root')
+function createRouteHandler() {
+  // Get the current location.
+  const currentLocation = history.location;
+  const [getLocation, setLocation] = createSignal(currentLocation.pathname);
+  const unlisten = history.listen((location, action) => {
+    // location is an object like window.location
+    setLocation(location.pathname);
+    console.log(action, location.pathname, location.state);
+  });
+  onCleanup(() => unlisten());
+  return match => match === getLocation();
+}
+
+
+const Home = () => (
+  <>
+    <h1>Welcome to this Simple Routing Example</h1>
+    <p>Click the links in the Navigation above to load different routes.</p>
+  </>
 );
+
+const Profile = () => (
+  <>
+    <h1>Your Profile</h1>
+    <p>This section could be about you.</p>
+  </>
+);
+
+const Settings = () => (
+  <>
+    <h1>Settings</h1>
+    <p>All that configuration you never really ever want to look at.</p>
+  </>
+);
+
+
+const NestedComponent = () => {
+  const [router] = useRouter();
+
+  // prettier-ignore
+  return (
+    <>
+      <p>{(router && router.history.location.pathname)}</p>
+    </>
+  );
+};
+
+const App = () => {
+  const matches = createRouteHandler();
+
+  return (
+    <>
+      <Router history={history}>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          <li>
+            <Link to='/settings'>Settings</Link>
+          </li>
+        </ul>
+        <Switch>
+        <Match when={( matches('/')  )}>
+          <Home />
+          <NestedComponent></NestedComponent>
+        </Match>
+        <Match when={( matches('/profile') )}>
+          <Profile />
+          <NestedComponent></NestedComponent>
+        </Match>
+        <Match when={( matches('/settings') )}>
+          <Settings />
+          <NestedComponent></NestedComponent>
+        </Match>
+      </Switch>
+      </Router>
+
+
+    </>
+  );
+};
+
+render(App, document.getElementById("app"));
